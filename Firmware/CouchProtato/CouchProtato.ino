2de33f146a17bdb7d319272b365bb2453f1465d1
unknown <beilers@purdue.edu>
2020-07-15 22:28:54 -0400

    Added libraries to code

--- Firmware/CouchProtato/CouchProtato.ino
+++ Firmware/CouchProtato/CouchProtato.ino
@@ -1,4 +1,12 @@
 #include "CouchProtatoPower.h"
+#include "AD524X.h"
+#include "Adafruit_SSD1306.h"
+#include "RTClib.h"
+#include "Adafruit_nRF52_Arduino.h"
+#include "OneButton.h"
+#include "SparkFun_LSM6DS3_Arduino_Library.h"
+#include "SparkFun_Ublox_Arduino_Library.h"
+#include "RN_52.h"
 
 CouchProtatoPower pwr;
 
